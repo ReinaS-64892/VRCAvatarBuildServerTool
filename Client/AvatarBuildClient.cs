6729b7be579a9f15a27b4dfc94e4ea51e2958847
Reina_Sakiria <rs@rs64.net>
2025-03-12 02:17:15 +0900

    feat: GetPrefabFromCAU

--- Client/AvatarBuildClient.cs
+++ Client/AvatarBuildClient.cs
@@ -1,4 +1,6 @@
 using System;
+using System.Collections;
+using System.Collections.Generic;
 using System.IO;
 using System.IO.Compression;
 using System.Linq;
@@ -8,6 +10,9 @@ using System.Threading.Tasks;
 using net.rs64.VRCAvatarBuildServerTool.Transfer;
 using UnityEditor;
 using UnityEngine;
+#if CAU
+using Anatawa12.ContinuousAvatarUploader.Editor;
+#endif
 
 namespace net.rs64.VRCAvatarBuildServerTool.Client
 {
@@ -20,41 +25,113 @@ namespace net.rs64.VRCAvatarBuildServerTool.Client
         [MenuItem("GameObject/VRCAvatarBuildServerTool/BuildToServer")]
         public static async void Do()
         {
-            var prefab = Selection.activeGameObject;
-            // VRCSDK の参照するのをサボっている。
-            if (prefab.GetComponent<Animator>() == null) { return; }
-
-            var isAsset = AssetDatabase.Contains(prefab);
-            Debug.Log("That Contains-AssetDataBase is" + isAsset);
-
-            var builded = UnityEngine.Object.Instantiate(prefab);
-#if NDMF
-            try
+            switch (Selection.activeObject)
             {
-                if (AvatarBuildClientConfiguration.instance.ClientSideNDMFExecution) nadena.dev.ndmf.AvatarProcessor.ProcessAvatar(builded);
-            }
-            catch (Exception e) { Debug.LogException(e); }
+                default: { Debug.Log("Unknown Target"); return; }
+                case GameObject gameObject:
+                    {
+                        var avatarRoot = gameObject;
+                        // VRCSDK の参照するのをサボっている。
+                        if (avatarRoot.GetComponent<Animator>() == null) { return; }
+                        EditorUtility.DisplayProgressBar("AvatarBuildClient-SentToBuild", "CloneAndBuildToAsset", 0f);
+
+                        var targetPath = CloneAndBuildToAsset(avatarRoot);
+
+                        EditorUtility.DisplayProgressBar("AvatarBuildClient-SentToBuild", "Post data prepare", 0.7f);
+
+                        var targetGUID = AssetDatabase.AssetPathToGUID(targetPath);
+                        var transferAssets = GetDependenciesWithFiltered(targetPath);
+
+                        try
+                        {
+                            var internalBinary = AssetTransferProtocol.EncodeAssetsAndTargetGUID(transferAssets, new string[] { targetGUID });
+                            EditorUtility.DisplayProgressBar("AvatarBuildClient-SentToBuild", "POST", 0.95f);
+                            await PostInternalBinary(internalBinary).ConfigureAwait(false);
+                        }
+                        finally
+                        {
+                            AssetDatabase.DeleteAsset(targetPath);
+                            EditorUtility.DisplayProgressBar("AvatarBuildClient-SentToBuild", "Exit", 1f);
+                        }
+                        Debug.Log("Exit Build transfer");
+                        EditorUtility.ClearProgressBar();
+                        return;
+                    }
+#if CAU
+                case AvatarUploadSettingOrGroup aus:
+                    {
+                        EditorUtility.DisplayProgressBar("AvatarBuildClient-SentToBuild", "GetPrefabs from cAU", 0f);
+                        var targetAvatarRoots = GetPrefabFromCAU(aus);
+
+                        EditorUtility.DisplayProgressBar("AvatarBuildClient-SentToBuild", "CloneAndBuildToAsset", 0.1f);
+                        var targetPaths = targetAvatarRoots.Select(CloneAndBuildToAsset).ToArray();
+
+                        EditorUtility.DisplayProgressBar("AvatarBuildClient-SentToBuild", "Post data prepare", 0.7f);
+                        var targetGUIDs = targetPaths.Select(AssetDatabase.AssetPathToGUID);
+                        var transferAssets = GetDependenciesWithFiltered(targetPaths);
+                        try
+                        {
+                            var internalBinary = AssetTransferProtocol.EncodeAssetsAndTargetGUID(transferAssets, targetGUIDs);
+                            EditorUtility.DisplayProgressBar("AvatarBuildClient-SentToBuild", "POST", 0.95f);
+                            await PostInternalBinary(internalBinary).ConfigureAwait(false);
+                        }
+                        finally
+                        {
+                            foreach (var targetPath in targetPaths) AssetDatabase.DeleteAsset(targetPath);
+                            EditorUtility.DisplayProgressBar("AvatarBuildClient-SentToBuild", "Exit", 1f);
+                        }
+                        Debug.Log("Exit Build transfer");
+                        EditorUtility.ClearProgressBar();
+                        return;
+                    }
 #endif
-            var targetPath = AssetDatabase.GenerateUniqueAssetPath("Assets/" + builded.name + ".prefab");
-
-            byte[] internalBinary = null;
-            try
-            {
-                PrefabUtility.SaveAsPrefabAsset(builded, targetPath);
-                var targetGUID = AssetDatabase.AssetPathToGUID(targetPath);
-
-                //ここで一部の依存関係が正しく include されない Shader とかの類を その AvatarRoot にコンポーネントをつけさせる形で追加できるといいね
-
-                var transferAssets = GetDependenciesWithFiltered(targetPath);
-
-                internalBinary = AssetTransferProtocol.EncodeAssetsAndTargetGUID(transferAssets, new string[] { targetGUID });
             }
-            finally
-            {
-                UnityEngine.Object.DestroyImmediate(builded);
-                AssetDatabase.DeleteAsset(targetPath);
-            }
+        }
 
+#if CAU
+        private static List<GameObject> GetPrefabFromCAU(AvatarUploadSettingOrGroup aus, List<GameObject> avatarRoots = null)
+        {
+            avatarRoots ??= new();
+            switch (aus)
+            {
+                default: break;
+                case AvatarUploadSetting avatarUploadSetting:
+                    {
+                        if (avatarUploadSetting.GetCurrentPlatformInfo().enabled is false) { break; }
+                        var avatarRoot = avatarUploadSetting.avatarDescriptor.asset as Component;
+                        if (avatarRoot == null)
+                        {
+                            if (avatarUploadSetting.avatarDescriptor.asset != null)
+                            { Debug.Log("on Scene Prefab is not supported"); }
+                            break;
+                        }
+
+                        avatarRoots.Add(avatarRoot.gameObject);
+                        break;
+                    }
+                case AvatarUploadSettingGroup group:
+                    {
+                        foreach (var a in group.avatars)
+                        {
+                            GetPrefabFromCAU(a, avatarRoots);
+                        }
+                        break;
+                    }
+                case AvatarUploadSettingGroupGroup groupGroup:
+                    {
+                        foreach (var g in groupGroup.groups)
+                        {
+                            GetPrefabFromCAU(g, avatarRoots);
+                        }
+                        break;
+                    }
+            }
+            return avatarRoots;
+        }
+#endif
+
+        private static async Task PostInternalBinary(byte[] internalBinary)
+        {
             try
             {
                 _client ??= new HttpClient() { Timeout = TimeSpan.FromSeconds(5) };
@@ -66,10 +143,25 @@ namespace net.rs64.VRCAvatarBuildServerTool.Client
             {
                 Debug.LogException(e);
             }
-            Debug.Log("Exit");
         }
 
-        private static string[] GetDependenciesWithFiltered(string targetPrefabPath)
+        private static string CloneAndBuildToAsset(GameObject avatarRoot)
+        {
+            var builded = UnityEngine.Object.Instantiate(avatarRoot);
+#if NDMF
+            try
+            {
+                if (AvatarBuildClientConfiguration.instance.ClientSideNDMFExecution) nadena.dev.ndmf.AvatarProcessor.ProcessAvatar(builded);
+            }
+            catch (Exception e) { Debug.LogException(e); }
+#endif
+            var targetPath = AssetDatabase.GenerateUniqueAssetPath("Assets/" + builded.name + ".prefab");
+            PrefabUtility.SaveAsPrefabAsset(builded, targetPath);
+            UnityEngine.Object.DestroyImmediate(builded);
+            return targetPath;
+        }
+
+        private static string[] GetDependenciesWithFiltered(params string[] targetPrefabPath)
         {
             return AssetDatabase.GetDependencies(targetPrefabPath)
             .Where(path => path.StartsWith("Packages") is false || path.StartsWith("Packages/nadena.dev.ndmf/__Generated"))
