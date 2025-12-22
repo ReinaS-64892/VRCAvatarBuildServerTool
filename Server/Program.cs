6d8808d33d4cff24c34de5205355fd43d3cad5a4
Reina_Sakiria <rs@rs64.net>
2025-12-22 14:38:49 +0900

    feat: improve path finding a instance.

--- Server/Program.cs
+++ Server/Program.cs
@@ -1,8 +1,33 @@
 ﻿using net.rs64.VRCAvatarBuildServerTool.Server;
 
+
+string instancePath;
+var configFileName = "config.json";
+
+if (File.Exists(configFileName))
+{
+    // current directory
+    Console.WriteLine("use current directory. in config  ./config.json");
+    instancePath = Path.GetFullPath(".");
+}
+else if (File.Exists("Server.csproj") && File.Exists("../ServerInstance/config.json"))
+{
+    Console.WriteLine("use project directory found config in ../ServerInstance/config.json");
+    instancePath = Path.GetFullPath("../ServerInstance");
+}
+else
+{
+    Console.WriteLine("config.json not found");
+    Console.WriteLine("dotnet run in VRCAvatarBuildServerTool/Server and make VRCAvatarBuildServerTool/ServerInstance and VRCAvatarBuildServerTool/ServerInstance/config.json");
+    Console.WriteLine("or create empty directory and then make config.json");
+    return;
+}
+
+Console.WriteLine("---");
+Console.WriteLine("instance path : " + instancePath);
+Console.WriteLine("config path   : " + Path.Combine(instancePath, configFileName));
+Console.WriteLine("---");
+
 Console.WriteLine("VRCAvatarBuildServer run !");
-var VABSToolDirectory = Path.Combine(Directory.GetCurrentDirectory(), "../");
-var instancePath = Path.Combine(VABSToolDirectory, "ServerInstance");
-AvatarBuildServer.UnityBuildRunnerPath = Path.Combine(VABSToolDirectory, "UnityBuildRunner");
 var abs = new AvatarBuildServer(instancePath);
 await abs.Run();
